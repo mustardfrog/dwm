e17bc2526c60964025fc2dada5762274c6a53079
Frog <frogdoctor@tutanota.com>
2022-04-12 14:35:56 +0630

    removed atom and qute command

--- config.def.h
+++ config.def.h
@@ -4,10 +4,10 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 28;       /* snap pixel */
 static const int scalepreview       = 1;        /* Tag preview scaling */
-static const unsigned int gappih    = 1;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 1;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 1;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 1;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -18,8 +18,9 @@ enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always
 static const int showtab			= showtab_auto;        /* Default tab bar show mode */
 static const int toptab				= True;               /* False means bottom tab bar */
 
-static const char *fonts[]          = {  "Monospace: size=10","Ubuntu:size=10", "fontawesome: size=14" };
-static const char dmenufont[]       = "Ubuntu: size=10";
+//static const char *fonts[]          = {  "Input Mono: size=10", "fontawesome: size=14" };
+static const char *fonts[]          = {  "Input Mono: size=10" , "Font Awesome: size=19"};
+static const char dmenufont[]       = "Ubuntu Mono: size=13";
 static const char col_gray1[]       = "#202020";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -27,7 +28,7 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray4, col_gray1, col_cyan },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
@@ -47,8 +48,6 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Zoom",     NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Atom",     NULL,       NULL,       1,            0,           -1 },
-	{ "qutebrowser",     NULL,       NULL,       1 << 2,            0,           -1 },
 };
 
 /* layout(s) */
@@ -93,19 +92,15 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *atomcmd[]  = { "atom", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *firefoxcmd[]  = { "librewolf", NULL };
-static const char *qutecmd[]  = { "qutebrowser", NULL };
 #include "shiftview.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY4,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY4,                       XK_Return, spawn,          {.v = termcmd } },
-	{ ControlMask|MODKEY4,           XK_a,       spawn,          {.v = atomcmd } },
 	{ ControlMask|MODKEY4,           XK_f,       spawn,          {.v = firefoxcmd } },
-	{ ControlMask|MODKEY4,           XK_s,       spawn,          {.v = qutecmd } },
 	{ MODKEY4,                       XK_b,      togglebar,      {0} },
 	{ MODKEY4|ShiftMask,             XK_w,      tabmode,        {-1} },
 	{ MODKEY4,                       XK_c,      focusstack,     {.i = +1 } },
