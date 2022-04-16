e9e8f209d208fd9c1b942c3194af8c986d80bbdc
Frog <frogdoctor@tutanota.com>
2022-04-16 14:00:27 +0630

    changed color

--- config.def.h
+++ config.def.h
@@ -2,14 +2,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 28;       /* snap pixel */
+static const unsigned int snap      = 30;       /* snap pixel */
 static const int scalepreview       = 1;        /* Tag preview scaling */
-static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const unsigned int gappih    = 1;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 1;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
@@ -19,13 +19,13 @@ static const int showtab			= showtab_auto;        /* Default tab bar show mode *
 static const int toptab				= True;               /* False means bottom tab bar */
 
 //static const char *fonts[]          = {  "Input Mono: size=10", "fontawesome: size=14" };
-static const char *fonts[]          = {  "Input Mono: size=10" , "Font Awesome: size=19"};
+static const char *fonts[]          = { "Ubuntu Mono: size=13"," Font Awesome: size=19"};
 static const char dmenufont[]       = "Ubuntu Mono: size=13";
-static const char col_gray1[]       = "#202020";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#000000";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#b4be82";
+static const char col_gray3[]       = "#89e8dd";
+static const char col_gray4[]       = "#89b8c2";
+static const char col_cyan[]        = "#2a3158";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray4, col_gray1, col_cyan },
@@ -94,7 +94,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *firefoxcmd[]  = { "librewolf", NULL };
 #include "shiftview.c"
 
